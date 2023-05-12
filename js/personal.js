3b6bec73a9438a6c9138bc2ce3b1560e08f35ea7
Ravindra Kumar Tilotia <ravindrakumartilotia4@gmail.com>
2023-05-12 19:13:50 +0530

    portfolio

--- js/personal.js
+++ js/personal.js
@@ -11,45 +11,36 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.display = "none";
 }
-
 function scrollFunction1() {
   let e = document.getElementById("home");
   e.scrollIntoView({
     behavior: "smooth",
   });
 }
+
 function scrollFunction2() {
   let e = document.getElementById("about");
-  // This ends the block to the window
-  // bottom and also aligns the view to the center
-  // console.log("s");
   e.scrollIntoView({
     behavior: "smooth",
   });
 }
+
 function scrollFunction3() {
   let e = document.getElementById("skills");
-  // This ends the block to the window
-  // bottom and also aligns the view to the center
-  console.log("s");
   e.scrollIntoView({
     behavior: "smooth",
   });
 }
+
 function scrollFunction4() {
   let e = document.getElementById("projects");
-  // This ends the block to the window
-  // bottom and also aligns the view to the center
-  console.log("s");
   e.scrollIntoView({
     behavior: "smooth",
   });
 }
+
 function scrollFunction5() {
   let e = document.getElementById("contact");
-  // This ends the block to the window
-  // bottom and also aligns the view to the center
-  console.log("s");
   e.scrollIntoView({
     behavior: "smooth",
   });
